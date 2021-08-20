083e56149f30d3a7c58bf01c3710a725f8290c4c
jamesiweobi <60060852+jamesiweobi@users.noreply.github.com>
2021-08-20 16:38:28 +0100

    made it better by hosting on heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const morgan = require('morgan');
 const PORT = process.env.PORT || 3000;
+const myName = 'James Iweobi';
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
@@ -13,7 +14,7 @@ app.post('/contact-me', (req, res) => {
   const { name, email, message } = req.body;
   res.status(200).json({
     status: 'success',
-    message: 'Thank you for reaching out!',
+    message: `Thank you for reaching out to ${myName}.`,
     data: { name, email, message },
   });
 });
